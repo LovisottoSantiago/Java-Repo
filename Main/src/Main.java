4110b49562301debca00e2cc939a95f043ca62fd
agussssbrr <agustinbaccari2426@gmail.com>
2024-07-06 15:35:29 -0300

    06/07
    creo clase person, utilizo funciones, pido info al usuario y ademas uso basico del if

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -4,20 +4,34 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+
+
+
+
+        //!USO CLASE PERSONA Y SU FUNCION PARA MOSTAR INFO
+        //!Podemos observar el funcionamiento del if y ademas le pedimos al usuario que ingrese info
+        /*
         Scanner input = new Scanner(System.in);
+        System.out.print("ingrese su nombre ");
+        String nombre = input.nextLine();
 
-        System.out.println("Hello World!");
-        System.out.print("Enter your name: ");
-        String name = input.nextLine();
-        System.out.println("Your name is " + name);
+        System.out.print("ingrese su apellido ");
+        String Apellido = input.nextLine();
 
-        if ("joselu".equalsIgnoreCase(name)) {
-            System.out.println("Welcome!");
+        System.out.print("ingrese su Edad ");
+        Integer edad = input.nextInt();
+
+        Persona Agustin= new Persona(nombre,Apellido,edad);
+        Agustin.verNombre();
+
+        if (nombre.equalsIgnoreCase("agus")) {
+            System.out.println("Bienvenido"+ nombre);
         } else {
-            System.out.println("Bye!");
-        }
+            System.out.println("Adiosss");
+        }*/
 
-
+            
     }
 
 }
--- Main/src/Persona.java
+++ Main/src/Persona.java
@@ -1,3 +1,20 @@
 public class Persona {
-    
+    private String Nombre;
+    private String Apellido ;
+    private int Edad;
+
+    public Persona (String Nombre, String Apellido, int Edad){
+        this.Nombre=Nombre;
+        this.Apellido=Apellido;
+        this.Edad=Edad  ;
+
+    }
+
+    public void verNombre(){
+        System.out.println(Nombre);
+        System.out.println(Apellido);
+        System.out.println(Edad);
+    }
+
+
 }
