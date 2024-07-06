aa0585f79cbd42e5b16e0d805690461735c2d215
LovisottoSantiago <santiagolovisotto@gmail.com>
2024-07-05 23:54:44 -0300

    Cambios

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -4,22 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Hello World!");
-        System.out.print("Enter your name: ");
-        String name = input.nextLine();
-        System.out.println("Your name is " + name);
-
-        if ("joselu".equalsIgnoreCase(name)) {
-            System.out.println("Welcome!");
-        } else {
-            System.out.println("Bye!");
-        }
-
+        alumno pablo = new alumno("Pablo", "Kevingston", 20);
+        pablo.saludar();
 
     }
-
 }
+
+
+    //! Pendientes:
+    //! -Herencia
+    //! -Polimorfismo
+    //! -Bucles
+
+
+
+
+
+
+
+
+
+
+
+
 
 
