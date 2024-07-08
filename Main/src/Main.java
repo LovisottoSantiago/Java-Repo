f08abb296a90a7eed1acff254d50a235e9744080
agussssbrr <agustinbaccari2426@gmail.com>
2024-07-08 15:57:08 -0300

    Utilizacion de GUI, y math para funciones matematicas

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import javax.swing.JOptionPane; //?USO PARA GUI
+
 
 public class Main {
 
@@ -6,6 +8,43 @@ public class Main {
 
 
 
+
+
+
+
+
+
+
+
+
+
+        //!UTILIZACION bASICA DE GUI,   USO IF, USO MATH(funcibes matematicas)
+        /*
+        Integer num_x = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer numero"));
+        Integer num_y = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo numero"));
+        JOptionPane.showMessageDialog(null,"Utilizo math, la division exacta entre los numeros es:" + Math.divideExact(num_x,num_y));
+
+
+
+        String nombre = JOptionPane.showInputDialog("Ingrese su nombre");
+        String apellido = JOptionPane.showInputDialog("Ingrese su Apellido");
+        JOptionPane.showMessageDialog(null,"Bienvenido:"+nombre+apellido);
+        Integer edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su edad"));
+
+        if (edad>=18){
+        JOptionPane.showMessageDialog(null,"Usted es mayo de edad, tiene :"+edad);
+        Integer DNI =Integer.parseInt(JOptionPane.showInputDialog("Ingrese su DNI"));
+        JOptionPane.showMessageDialog(null,"Su dni es :"+DNI);
+        JOptionPane.showMessageDialog(null,"Te damos la bienvenida a ChoChox, dale suave pape \uD83D\uDC7B");
+
+
+            }
+        else{
+            JOptionPane.showMessageDialog(null,"Usted es menor de edad, tiene :"+edad);
+            JOptionPane.showInputDialog(null,"Ya no puede seguir ingresando datos");
+        }
+
+        */
 
 
 
